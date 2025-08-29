cefe2d267e464cc5f202292a8cb65ac88388ec89
rAnjeel <rabearisonangelo21@gmail.com>
2025-08-29 16:00:58 +0300

    Order by id Ag-grid list

--- src/credentials/credentials.service.ts
+++ src/credentials/credentials.service.ts
@@ -139,7 +139,7 @@ export class CredentialsService implements NestMiddleware {
           ON h1.siteId = h2.siteId AND h1.connectionErrorDate = h2.maxDate
         ) AS latest_historic
         ON cs.id = latest_historic.siteId
-        WHERE latest_historic.errorStatus = 'unresolved' GROUP BY cs.id;
+        WHERE latest_historic.errorStatus = 'unresolved' ORDER BY cs.id;
       `);
 
     return result;
